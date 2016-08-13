9d65caf2cb0372cc11a78f415fe4db5de17a7cdc
Jacob Oliver <jacob@systemed.co.uk>
2016-08-13 14:47:05 +0100

    Update Ubuntu-Install.sh

--- Installers/Ubuntu-Install.sh
+++ Installers/Ubuntu-Install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 echo Please enter your password to continue...
-sudo su
 
 # Install the following dependencies from repositories without asking questions, git-core can be removed afterwords as it's only used to initialise a local git repository
 echo Installing dependencies
-apt-get -y install git-core python3 cython3 libusb python3-pip
+sudo apt-get -y install git-core python3 cython3 libusb python3-pip
 
 # Install the hidapi dependency
-pip install hidapi
+sudo pip install hidapi
 
 # Get the sudoers home directory before changing directory into it
 eval echo "~$SUDO_USER" && cd -
@@ -22,6 +21,6 @@ git clone git://github.com/Ban3/python-evic.git ./python-evic && cd ./python-evi
 
 # Install python-evic
 echo Installing python-evic
-python3 setup.py install
+sudo python3 setup.py install
 
 echo Installation complete. Run \"sudo evic-usb upload /path/to/firmware.bin\" to install a new firmware.
